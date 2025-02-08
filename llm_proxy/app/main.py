76a07e477f9074f59d1ed3421dab1d773eb248da
zkud <anton.kudyrka@gmail.com>
2025-02-08 23:52:02 +0300

    feat: docker compose up config for llm_proxy

--- llm_proxy/app/main.py
+++ llm_proxy/app/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(router)
 
 if __name__ == '__main__':
-    run(app, host='127.0.0.1', port=8000, log_level='info')
+    run(app, host='0.0.0.0', port=8000, log_level='info')
